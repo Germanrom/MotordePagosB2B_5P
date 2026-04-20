2a1417db7b874a6f027e18693572558c0068bca5
German Romero Severino <germanromeroseverino@MacBook-Air-de-German.local>
2026-04-20 14:39:47 -0300

    Agregada ruta callback para Mercado Pago OAuth

--- main.py
+++ main.py
@@ -1,8 +1,54 @@
+import os
+import requests # <-- Importante
 from fastapi import FastAPI, Depends
 from sqlalchemy import Column, Integer, String, Float
 from sqlalchemy.orm import Session
 from src.infrastructure.database.db_config import engine, get_db, Base
 from src.infrastructure.mercadopago.adaptador import generar_link_de_pago
+
+# --- 1. NUEVA TABLA: VENDEDORES ---
+class Vendedor(Base):
+    __tablename__ = "vendedores"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String, default="Comercio Generico")
+    mp_access_token = Column(String)  
+    mp_refresh_token = Column(String) 
+
+Base.metadata.create_all(bind=engine)
+
+# --- 2. LA RUTA RECEPTORA (OAUTH) ---
+@app.get("/callback")
+def mercadopago_callback(code: str, db: Session = Depends(get_db)):
+    url = "https://api.mercadopago.com/oauth/token"
+    datos_para_mp = {
+        "client_secret": os.getenv("MP_CLIENT_SECRET"),
+        "client_id": os.getenv("MP_CLIENT_ID"),
+        "grant_type": "authorization_code",
+        "code": code,
+        # 👇 Esta URL DEBE coincidir con la que pusiste en Mercado Pago
+        "redirect_uri": "https://motor-de-pagos-api.onrender.com/callback" 
+    }
+    
+    respuesta = requests.post(url, data=datos_para_mp)
+    tokens = respuesta.json()
+    
+    if "access_token" not in tokens:
+        return {"status": "error", "mensaje": "Falló la vinculación", "detalle": tokens}
+        
+    nuevo_vendedor = Vendedor(
+        mp_access_token=tokens["access_token"],
+        mp_refresh_token=tokens["refresh_token"]
+    )
+    db.add(nuevo_vendedor)
+    db.commit()
+    db.refresh(nuevo_vendedor)
+    
+    return {
+        "status": "success",
+        "mensaje": "✅ ¡Cuenta de Mercado Pago vinculada perfectamente!",
+        "vendedor_id_interno": nuevo_vendedor.id
+    }
 
 # 1. Definimos la estructura EXACTA de tu tabla en Supabase
 class Orden(Base):
