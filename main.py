eee61f5c4d7c038584deb9ddedac9d56c55177fb
German Romero Severino <germanromeroseverino@MacBook-Air-de-German.local>
2026-04-21 16:30:19 -0300

    Fix: Configuración de CORS agregada para permitir Frontend

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.infrastructure.database.db_config import engine, Base
 
-# Importamos las rutas desde su nueva carpeta
+# Importamos las rutas
 from routes.api_routes import router as api_router
 
 # 1. INICIALIZAMOS FASTAPI
 app = FastAPI(title="Motor de Pagos API", version="2.0.0")
 
+# --- NUEVO: CONFIGURACIÓN DE CORS (Permisos de frontera) ---
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # El "*" significa que permitimos que CUALQUIER web nos hable
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite GET, POST, PUT, DELETE
+    allow_headers=["*"],  # Permite enviar la API Key secreta
+)
+# -----------------------------------------------------------
+
 # 2. SINCRONIZAMOS LA BASE DE DATOS
 Base.metadata.create_all(bind=engine)
 
-# 3. CONECTAMOS LAS RUTAS (Le decimos a FastAPI que use las puertas que creamos)
+# 3. CONECTAMOS LAS RUTAS
 app.include_router(api_router)
